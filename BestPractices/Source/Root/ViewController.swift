d0294eac014b698da981cf08c1872cee3fbf6fc0
Westin Newell <wnewell87@gmail.com>
2018-11-12 19:52:41 -0800

    Add UILabel that displays success text.

--- BestPractices/Source/Root/ViewController.swift
+++ BestPractices/Source/Root/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 import MyFramework
 import ReactiveCocoa
+import ReactiveSwift
 
 class ViewController: UIViewController {
 
@@ -9,9 +10,25 @@ class ViewController: UIViewController {
 
         self.view.backgroundColor = .red
 
-        ReactiveTester.signalProducer().startWithValues { (string) in
-            print(string)
-        }
+        let label = UILabel(frame: CGRect.zero)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+
+        self.view.addSubview(label)
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            label.leadingAnchor.constraint(lessThanOrEqualTo: self.view.leadingAnchor),
+            label.trailingAnchor.constraint(greaterThanOrEqualTo: self.view.leadingAnchor),
+            label.topAnchor.constraint(lessThanOrEqualTo: self.view.safeAreaLayoutGuide.topAnchor),
+            label.bottomAnchor.constraint(greaterThanOrEqualTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+
+        label.reactive.text <~ ReactiveTester.signalProducer().on(value: { text in
+            print(text)
+        })
     }
 
 }
--- MyFramework/Source/ReactiveTester.swift
+++ MyFramework/Source/ReactiveTester.swift
@@ -2,11 +2,13 @@ import ReactiveSwift
 import Result
 
 public class ReactiveTester {
+
     public class func signalProducer() -> SignalProducer<String, NoError> {
         return SignalProducer.timer(interval: DispatchTimeInterval.milliseconds(500), on: QueueScheduler.main)
             .take(first: 1)
             .map { _ in
-                return "This is a test"
+                return "It works!"
             }
     }
+    
 }
