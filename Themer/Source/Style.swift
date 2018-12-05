70d9e66c62babaebc7288d07182d8f76859c4705
Westin Newell <wnewell87@gmail.com>
2018-12-05 13:30:37 -0800

    Removed unused protocol.

--- Themer/Source/Style.swift
+++ Themer/Source/Style.swift
@@ -3,28 +3,3 @@ public protocol Style {
     associatedtype Styleable
     func apply(to styleable: Styleable)
 }
-
-/// A struct or class that can have a style applied to it.
-///
-/// This is typically a UIView or UIViewController subclass.
-public protocol StyleApplicable {
-    associatedtype StyleType: Style where StyleType.Styleable == Self
-
-    /// The theme type of the style.
-    ///
-    /// This is typically an enum (e.g. .dark/.light).
-    associatedtype ThemeType
-
-    /// Should return a style for the provided theme that can be applied to the
-    /// receiver.
-    func makeStyleWithTheme(_ theme: ThemeType) -> StyleType
-}
-
-public extension StyleApplicable {
-
-    /// Applys the provided style to the receiver.
-    public func applyStyle(_ style: StyleType) {
-        style.apply(to: self)
-    }
-
-}
