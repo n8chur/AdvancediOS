522aa2e7fe2cba914ea930f435843a7f76ce57d1
Westin Newell <n8chur@users.noreply.github.com>
2018-12-08 21:55:27 -0800

    Add logging framework. (#42)
    * Update dependencies.
    
    Additionally fixed a typo in the Cartfile.
    
    * Add Logger.
    
    * Improve documentation.
    
    * Fix CocoaLumberjack Rome repository map.
    
    * Use new instance instead of sharedInstance for DDOSLogger.

---
+++ Logger/Source/Log.swift
@@ -0,0 +1,81 @@
+import CocoaLumberjackSwift
+
+public struct Log {
+
+    public func verbose(_ message: @autoclosure () -> String, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+        Logger.shared.log(.verbose, message, file: file, function: function, line: line)
+    }
+
+    static public func debug(_ message: @autoclosure () -> String, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+        Logger.shared.log(.debug, message, file: file, function: function, line: line)
+    }
+
+    static public func info(_ message: @autoclosure () -> String, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+        Logger.shared.log(.info, message, file: file, function: function, line: line)
+    }
+
+    static public func warn(_ message: @autoclosure () -> String, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+        Logger.shared.log(.warn, message, file: file, function: function, line: line)
+    }
+
+    static public func error(_ message: @autoclosure () -> String, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+        Logger.shared.log(.error, message, file: file, function: function, line: line)
+    }
+
+}
+
+/// A logger intended to be used as a singleton.
+public struct Logger {
+
+    public static let shared = Logger()
+
+    private let fileLogger: DDFileLogger
+
+    private init() {
+        let logFormatter = LogFormatter()
+
+        fileLogger = DDFileLogger()
+        fileLogger.rollingFrequency = 60 * 60 * 24 // 24 hours
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+        fileLogger.logFormatter = logFormatter
+        DDLog.add(fileLogger, with: .info)
+
+        let osLogger = DDOSLogger()
+        osLogger.logFormatter = logFormatter
+        DDLog.add(osLogger)
+
+        // Do not show verbose or debug logs in non-debug builds.
+        #if DEBUG
+            dynamicLogLevel = .all
+        #else
+            dynamicLogLevel = .info
+        #endif
+    }
+
+    public func log(_ level: LogLevel, _ message: @autoclosure () -> String, file: StaticString = #file, function: StaticString = #function, line: UInt = #line) {
+        switch level {
+        case .verbose: DDLogVerbose(message, file: file, function: function, line: line)
+        case .debug: DDLogDebug(message, file: file, function: function, line: line)
+        case .info: DDLogInfo(message, file: file, function: function, line: line)
+        case .warn: DDLogWarn(message, file: file, function: function, line: line)
+        case .error: DDLogError(message, file: file, function: function, line: line)
+        }
+    }
+
+    /// Returns an array of all existing log files.
+    ///
+    /// This could be used to upload log files for a support ticket or a crash report.
+    public var logFileURLs: [URL] {
+        return fileLogger.logFileManager.unsortedLogFilePaths
+            .map { URL.init(fileURLWithPath: $0, isDirectory: false) }
+    }
+
+}
+
+public enum LogLevel {
+    case verbose
+    case debug
+    case info
+    case warn
+    case error
+}
