a8c47653a2d4315a57be9890b04c5bf81962bca4
Westin Newell <n8chur@users.noreply.github.com>
2018-12-10 15:47:45 -0800

    Use XCGLogger for logging. (#44)
    * Use XCGLogger for logging.
    
    * Rename formatter to ContextFormatter.
    
    * Use os_log for console logging.
    
    * Update Romefile.
    
    * Fix Rome repository map for ReactiveCocoa
    
    * Fix typo in Romefile.
    
    * Revert ReactiveCocoa map.
    
    This is only necessary when:
    - The repository’s name does not match the built framework’s name
    - The repository builds mulitple frameworks from the same project
    ReactiveCocoa has external dependencies and only one framework built from that repository’s project, and therefore this map is not necessary.
    
    * Fix dev workspace.

---
+++ Logger/Source/ContextFormatter.swift
@@ -0,0 +1,28 @@
+import XCGLogger
+
+/// Formats XCGLogger messages.
+///
+/// Prefixes all messages with the flag name.
+class ContextFormatter: LogFormatterProtocol {
+
+    static let userInfoKey = "Context"
+
+    let contextManager: Logger.ContextManager
+
+    init(contextManager: Logger.ContextManager) {
+        self.contextManager = contextManager
+    }
+
+    func format(logDetails: inout LogDetails, message: inout String) -> String {
+        guard let context = logDetails.userInfo[ContextFormatter.userInfoKey] as? Logger.Context else {
+            return message
+        }
+        message = "<\(context.identifier)> \(message): "
+        return message
+    }
+
+    var debugDescription: String {
+        return "\(type(of: self)): "
+    }
+
+}
