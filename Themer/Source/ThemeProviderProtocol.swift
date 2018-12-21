651172914b7f72b7f827a5382a4c9c6efa64dc0c
Westin Newell <wnewell87@gmail.com>
2018-12-21 14:57:33 -0800

    Convert Themer to RxSwift.

--- Themer/Source/ThemeProviderProtocol.swift
+++ Themer/Source/ThemeProviderProtocol.swift
@@ -1,5 +1,5 @@
-import ReactiveCocoa
-import ReactiveSwift
+import RxCocoa
+import RxSwift
 
 /// A protocol describing a class that can apply a themes to objects (e.g. a view).
 public protocol ThemeProviderProtocol {
@@ -9,7 +9,7 @@ public protocol ThemeProviderProtocol {
     associatedtype ThemeType
 
     /// The current theme.
-    var theme: MutableProperty<ThemeType> { get }
+    var theme: Variable<ThemeType> { get }
 }
 
 public extension ThemeProviderProtocol {
@@ -18,17 +18,17 @@ public extension ThemeProviderProtocol {
     ///
     /// A new style is created using the provided closure whenever the theme changes, and
     /// is then applied to the styleable.
-    public func bindToStyleable<StyleType: Style>(_ styleable: StyleType.Styleable, makeStyle: @escaping (ThemeType) -> StyleType) where StyleType.Styleable: AnyObject {
-        theme.producer
-            .take(duringLifetimeOf: styleable)
-            .startWithValues { [weak styleable] theme in
+    public func bindToStyleable<StyleType: Style>(_ styleable: StyleType.Styleable, makeStyle: @escaping (ThemeType) -> StyleType) where StyleType.Styleable: AnyObject & ReactiveCompatible {
+        _ = theme.asObservable()
+            .takeUntil(styleable.rx.deallocated)
+            .subscribe(onNext: { [weak styleable] theme in
                 guard let styleable = styleable else {
                     return
                 }
 
                 let style = makeStyle(theme)
                 style.apply(to: styleable)
-            }
+            })
     }
 
 }
